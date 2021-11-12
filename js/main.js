6c7c695c759737b30a415ebf00f534d542efd3ec
chris619s <52170775+chris619s@users.noreply.github.com>
2021-11-12 16:23:45 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+  //common reveal options to create reveal animations
+  ScrollReveal({
+    //reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+  });
+
+  //target elements, and specify options to create reveal animations
+  ScrollReveal().reveal('.main-title, .section-title, .h2' , { delay: 500, origin: 'left' });
+  ScrollReveal().reveal('.sec-01 .image, .info, .carousel-inner, .mySwiper ', { delay: 600, origin: 'bottom' });
+  ScrollReveal().reveal('.text-box', { delay: 700, origin: 'right' });
+  ScrollReveal().reveal('.media-icons i', { delay: 500, origin: 'bottom', interval: 200 });
+  ScrollReveal().reveal('.sec-02 .image, .sec-03 .image, .gallery_block', { delay: 500, origin: 'top' });
+  ScrollReveal().reveal('.media-info li, .cx-title', { delay: 500, origin: 'left', interval: 200 });
+ 
+
+ //For Swiper Effect //
+ 
